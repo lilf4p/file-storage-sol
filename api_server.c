89a506bdc661f50548bcd87b3d4da966d41ed5fc
lilf4p <leostoppa999@gmail.com>
2021-06-07 16:05:55 +0200

    implementata closeConnection api

--- api_server.c
+++ api_server.c
@@ -14,7 +14,9 @@
 #define SYSCALL(c,e) \
     if(c==-1) { errno=e;return -1; }
 
+int connesso = 0;//FLAG PER SAPERE SE MI SONO CONNESSO O NO 
 int sc; //SOCKET FD
+char *socket_name;
 char response[N];
 
 //FUNZIONI DI UTILITA'
@@ -47,9 +49,30 @@ int openConnection(const char* sockname, int msec,const struct timespec abstime)
     SYSCALL(read(sc,response,N),EREMOTEIO);
     printf("%s\n",response);
     fflush(stdin);
+    connesso = 1;
+    socket_name = sockname;
     return 0;
 
 }
+
+//errore se sockname non corretto, o se non connesso 
+int closeConnection(const char* sockname) {
+
+    if (connesso==0) {
+        errno=EPERM;
+        return -1;
+    }
+
+    if (strcmp(socket_name,sockname)==0) {
+        SYSCALL(close(sc),EREMOTEIO);
+        printf ("Close connection...\n");
+        return 0;
+    }else{
+        errno=EINVAL;
+        return -1;
+    }
+}
+
 
 //-------------FUNZIONI DI UTILITY--------------//
 
